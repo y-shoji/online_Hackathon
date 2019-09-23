3678676f01616c70a931d96a1bc4be710b7bb365
nakagawa333 <naka33321@gmail.con>
2019-09-23 22:48:12 +0900

    LEDボタンを真中寄せ

---
+++ aruduino/serialLED/static/Index/index_setWidth.js
@@ -0,0 +1,31 @@
+//幅
+function checkWidth(){
+	let browserWidth = $(window).width();
+	let boxW = $("#LED").width();
+
+	let plusPxw = (( browserWidth - boxW) / 2);
+
+	$("#LED").css({"left":plusPxw + "px" });
+
+}
+
+//高さを真中に調整する
+function checkHeight(){
+	let browserHeight = $(window).height();
+	let boxH  =$("#LED").height();
+
+	let plusPxh = (( browserHeight - boxH) / 2);
+
+	$("#LED").css({"margin-top": plusPxh + "px"});
+}
+
+$(function(){
+	checkWidth();
+	checkHeight()
+})
+
+$(window).on("load resize",function(){
+	checkWidth();
+	checkHeight();
+})
+
