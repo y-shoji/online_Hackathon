fb85746eda9e4b6c5b003e15dc05e4f09c1ebc8c
nakagawa333 <naka33321@gmail.con>
2019-09-24 23:32:34 +0900

    WebsocketのURLに接続する処理

---
+++ aruduino/serialLED/static/weConnect.js
@@ -0,0 +1,25 @@
+//Websocketに接続する
+var connection = new WebSocket("");
+
+//通信が接続された場合
+
+connection.onopen = function(e){
+	connection.send("sample data");
+};
+
+//エラーが発生した場合
+connection.onerror = function(error){
+
+};
+
+//メッセージを受け取った場合
+connection.onmessage = function(e){
+
+};
+
+//通信が切断され
+connection.onclose = function(){
+	connection.close();
+};
+
+
